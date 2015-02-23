a1c333ecab0903756b4a8b43fb14bdbadf2d695c
jlcanela <jlcanelam@gmail.com>
2015-02-23 01:02:49 +0100

    adding source page

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,45 +1,54 @@
 (function() {
-  var app = angular.module('website', [ ]);
-  app.config(['$httpProvider', function ($httpProvider) {
-    //Reset headers to avoid OPTIONS request (aka preflight)
-    $httpProvider.defaults.headers.common = {};
-    $httpProvider.defaults.headers.post = {};
-    $httpProvider.defaults.headers.put = {};
-    $httpProvider.defaults.headers.patch = {};
-  }]);
+    var app = angular.module('website', [ ]);
+    app.config(['$httpProvider', function ($httpProvider) {
+        //Reset headers to avoid OPTIONS request (aka preflight)
+        $httpProvider.defaults.headers.common = {};
+        $httpProvider.defaults.headers.post = {};
+        $httpProvider.defaults.headers.put = {};
+        $httpProvider.defaults.headers.patch = {};
+    }]);
 
-  var wc = app.controller("WebsiteController", ['$http', function($http){ 
-    var ctrl = this;
-    
-    //ctrl.website = {};
-    ctrl.websites = [];
-    
-    $http.get('http://localhost:9000/websites').success(function(data){
-        console.log(data);
-        ctrl.websites = data;
-    });
-    
-    ctrl.addWebsite = function(){
-        console.log(ctrl.website);
-        
-        ctrl.website.id = 1;
-        ctrl.website.path = "";
-        
-        $http({
-            url: 'http://localhost:9000/websites',
-            dataType: 'json',
-            method: 'POST',
-            data: ctrl.website,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).success(function(data){
-            ctrl.websites.push(data);
-            ctrl.website = {};
+    var wc = app.controller("WebsiteController", ['$http', function($http){ 
+        var ctrl = this;
+
+        ctrl.websites = [];
+
+        $http.get('http://localhost:9000/websites').success(function(data){
+            console.log(data);
+            ctrl.websites = data;
         });
-    };
-    
-    
-  }]);
+
+        ctrl.addWebsite = function(){
+            console.log(ctrl.website);
+
+            ctrl.website.id = 1;
+            ctrl.website.path = "";
+
+            $http({
+                url: 'http://localhost:9000/websites',
+                dataType: 'json',
+                method: 'POST',
+                data: ctrl.website,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).success(function(data){
+                ctrl.websites.push(data);
+                ctrl.website = {};
+            });
+        };
+    }]);
+  
+    var sc = app.controller("SourceController", ['$http', function($http){ 
+        var ctrl = this;
+
+        ctrl.sources = [];
+
+        $http.get('http://localhost:9000/sources').success(function(data){
+            console.log(data);
+            ctrl.sources = data;
+        });
+
+    }]);
 }());
 
---
+++ app/controllers/SourceController.scala
@@ -0,0 +1,36 @@
+package controllers
+
+import api.WebsiteApi
+import models.Website
+import models.test.Source
+import play.api.libs.json._
+import play.api.mvc._
+import play.api.db._
+import play.api.Play.current
+import anorm._
+//import anorm._
+import play.api.Logger
+
+object SourceController extends Controller with ControllerHelper {
+
+  implicit val sourceFormat = Json.format[Source]
+  
+  def list() = Action {
+    implicit request =>
+      def res = DB.withConnection { implicit conn =>
+        // do whatever you need with the connection
+        // Create an SQL query
+        val selectSources = SQL("Select * from Source")
+
+        // Transform the resulting Stream[Row] to a List[(String,String)]
+        selectSources().map(row =>
+          row[String]("name") -> row[String]("url")
+        ).map { case (name, url) => Source(name, url) }.toList
+      }
+
+      Ok(Json.toJson(res)).withHeaders(("Access-Control-Allow-Origin", "*"));
+  }
+
+
+
+}
